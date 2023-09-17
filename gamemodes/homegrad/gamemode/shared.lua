603630949008133ee8c9d232e480f9581714d6cc
sekta <akovlevmihail535@gmail.com>
2023-09-18 02:19:33 +0400

    Modules

--- gamemodes/homegrad/gamemode/shared.lua
+++ gamemodes/homegrad/gamemode/shared.lua
@@ -1,1 +1,29 @@
 homegrad = homegrad or {}
+
+// Modules
+
+local sv = file.Find("homegrad/gamemode/modules/sv_*.lua","LUA")
+local sh = file.Find("homegrad/gamemode/modules/sh_*.lua","LUA")
+local cl = file.Find("homegrad/gamemode/modules/cl_*.lua","LUA")
+
+if SERVER then
+    for _,path in pairs(sv) do
+        homegrad.server("modules/" .. path)
+    end
+
+    for _,path in pairs(sh) do
+        homegrad.shared("modules/" .. path)
+    end
+
+    for _,path in pairs(cl) do
+        homegrad.client("modules/" .. path)
+    end
+else
+    for _,path in pairs(sh) do
+        homegrad.client("modules/" .. path)
+    end
+
+    for _,path in pairs(cl) do
+        homegrad.client("modules/" .. path)
+    end
+end
