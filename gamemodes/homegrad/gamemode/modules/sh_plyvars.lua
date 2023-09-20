b437bf53314fa0626a2c3d5a5590961147f4e0de
sekta <akovlevmihail535@gmail.com>
2023-09-20 15:49:27 +0400

    Added NYPD models and Enhanced Citizens

--- gamemodes/homegrad/gamemode/modules/sh_plyvars.lua
+++ gamemodes/homegrad/gamemode/modules/sh_plyvars.lua
@@ -1,6 +1,12 @@
 homegrad.limbs = {
-    ["head"] = "ValveBiped.Bip01_Head1",
-    ["neck"] = "ValveBiped.Bip01_Neck1",
+    ["head"] = {"ValveBiped.Bip01_Head1",HITGROUP_HEAD},
+    ["neck"] = {"ValveBiped.Bip01_Neck1",HITGROUP_HEAD},
+    ["chest"] = {"ValveBiped.Bip01_Neck1",HITGROUP_CHEST},
+    ["stomach"] = {"ValveBiped.Bip01_Neck1",HITGROUP_STOMACH},
+    ["leftarm"] = {"ValveBiped.Bip01_Neck1",HITGROUP_LEFTARM},
+    ["rightarm"] = {"ValveBiped.Bip01_Neck1",HITGROUP_RIGHTARM},
+    ["leftleg"] = {"ValveBiped.Bip01_Neck1",HITGROUP_LEFTLEG},
+    ["rightleg"] = {"ValveBiped.Bip01_Neck1",HITGROUP_RIGHTLEG},
 }
 
 local meta = FindMetaTable("Player")
