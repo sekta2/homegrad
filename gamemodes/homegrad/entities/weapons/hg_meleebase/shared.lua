bebb401331cdb630e57613d89ddd6c809002eafc
sekta <akovlevmihail535@gmail.com>
2023-09-28 20:12:26 +0400

    Good morning Vietnam

--- gamemodes/homegrad/entities/weapons/hg_meleebase/shared.lua
+++ gamemodes/homegrad/entities/weapons/hg_meleebase/shared.lua
@@ -9,7 +9,6 @@ SWEP.IsHomegrad = true
 SWEP.IsMelee = true
 
 SWEP.NextShoot = 0
-SWEP.ShootWait = 0.1
 
 SWEP.Primary.ClipSize = 50
 SWEP.Primary.DefaultClip = 50
@@ -18,19 +17,12 @@ SWEP.Primary.Ammo = "none"
 SWEP.Primary.Cone = 0
 SWEP.Primary.Damage = 100
 SWEP.Primary.Spread = 0
-SWEP.Primary.Sound = {}
-SWEP.Primary.SoundHit = {}
-SWEP.Primary.SoundHitWall = {}
-SWEP.Primary.SoundDraw = ""
 SWEP.Primary.Force = 0
 
 SWEP.Secondary.ClipSize    = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
-
-SWEP.ViewModel = "models/pwb/weapons/w_knife.mdl"
-SWEP.WorldModel = "models/pwb/weapons/w_knife.mdl"
 
 function SWEP:Initialize()
     self:SetHoldType("knife")
@@ -67,10 +59,10 @@ function SWEP:PrimaryAttack()
     if SERVER then
         if IsValid(tr.Entity) then
             if tr.Entity:IsPlayer() then
-                local snd = self.Primary.SoundHit[math.Rand(1,#self.Primary.SoundHit)]
+                local snd = self.Primary.SoundHit[math.random(1,#self.Primary.SoundHit)]
                 owner:EmitSound(snd)
             else
-                local snd = self.Primary.SoundHitWall[math.Rand(1,#self.Primary.SoundHitWall)]
+                local snd = self.Primary.SoundHitWall[math.random(1,#self.Primary.SoundHitWall)]
                 owner:EmitSound(snd)
             end
             tr.Entity:TakeDamage(self.Primary.Damage,owner,self)
