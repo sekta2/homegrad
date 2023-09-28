3233ac4eaaf215acf627e37c2602a64af9be58eb
sekta <akovlevmihail535@gmail.com>
2023-09-29 03:37:47 +0400

    лан я пошел спать

--- gamemodes/homegrad/gamemode/modules/cl_hud.lua
+++ gamemodes/homegrad/gamemode/modules/cl_hud.lua
@@ -20,6 +20,8 @@ hook.Add("HUDDrawPickupHistory","hg.hidehistory",function()
     return false
 end)
 
+local shownextround = 0
+
 hook.Add("HUDPaint","hg.hudpaint",function()
     local ply = LocalPlayer()
 
@@ -38,11 +40,19 @@ hook.Add("HUDPaint","hg.hudpaint",function()
     local maincolor = Color(255,255,255,math.Clamp(startRound - 0.5,0,1) * 255)
     local teamcolor = Color(tc.r,tc.g,tc.b,math.Clamp(startRound - 0.5,0,1) * 255)
 
+    shownextround = Lerp(2 * FrameTime(),shownextround or 0,0)
+
+    if shownextround > 0 then
+        draw.SimpleText("Текущий: " .. currentmode, "hg.big", scrw - 5, 5, Color(255,255,255,shownextround * 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
+        draw.SimpleText("Следущий: " .. nextmode, "hg.big", scrw - 5, 25, Color(255,255,255,shownextround * 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
+    end
+
+    if not roundstarted then shownextround = 3 end
+
     if startRound > 0 and ply:Alive() and roundstarted then
         ply:ScreenFade(SCREENFADE.IN,Color(0,0,0,255),3,0.5)
 
-        draw.SimpleText("Текущий: " .. currentmode, "hg.big", scrw - 5, 5, maincolor, TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
-        draw.SimpleText("Следущий: " .. nextmode, "hg.big", scrw - 5, 25, maincolor, TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
+        shownextround = 15
 
         draw.SimpleText(currentmode, "hg.big", scrw / 2, scrh / 8, maincolor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 
@@ -50,4 +60,8 @@ hook.Add("HUDPaint","hg.hudpaint",function()
         draw.SimpleText(yourole, "hg.big", scrw / 2, (scrh / 2) + 10, teamcolor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
         draw.SimpleText(youroledesc, "hg.big", scrw / 2, scrh / 1.2, teamcolor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
     end
+end)
+
+hook.Add("ScoreboardShow","hg.shownextround",function()
+    shownextround = 15
 end)
